ac99f34e9fb8326b5442426e09b66779bf88f0d4
Michael Wang <michaelw0395@gmail.com>
2019-08-20 15:29:19 +1000

    Completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    # render 'tasks/show'
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+    # redirect user back to home page
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(user_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Create
+  # 1. new
+  get "tasks/new", to: "tasks#new", as: :new_task
+  # 2. post
+  post "tasks", to: "tasks#create"
+  # Read
+  get "tasks", to: "tasks#index" # tasks becomes the prefix
+  get "tasks/:id", to: "tasks#show", as: :task
+  # Update
+  # 1. Get the form
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # 2. Patch the form
+  patch "tasks/:id", to: "tasks#update"
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
